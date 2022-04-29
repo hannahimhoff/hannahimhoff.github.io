b11b6a7fa65b04c5b62a0c4abcee5c744f8d4484
hannahimhoff <70405443+hannahimhoff@users.noreply.github.com>
2022-04-29 14:22:03 -0600

    fruit quiz update

--- js/quiz.js
+++ js/quiz.js
@@ -3,38 +3,144 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
-	var correct = 0;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	//var correct = 0;
+	let a = 0;
+	let b = 0;
+	let c = 0;
+	let d = 0;
+	let e = 0;
 
-
+// Q1
+	if (question1 == "avocado toast") {
+		a++;
+}
+	if (question1 == "pancakes") {
+		b++;
+}
+	if (question1 == "omelette") {
+		c++;
+}
+	if (question1 == "bacon & eggs") {
+		d++;
+}
 	if (question1 == "oatmeal") {
-		correct++;
+		e++;
 }
-	if (question2 == "Hartford") {
-		correct++;
+
+// Q2
+	if (question2 == "santorini") {
+			a++;
 }
-	if (question3 == "Albany") {
-		correct++;
-	}
+	if (question2 == "waikato") {
+			b++;
+}
+	if (question2 == "tokyo") {
+			c++;
+}
+	if (question2 == "hanoi") {
+			d++;
+}
+	if (question2 == "rome") {
+			e++;
+}
 
-	var pictures = ["../img/toby1.png", "../img/toby2.png", "../img/toby3.png"];
-	var messages = ["Great job!", "That's just okay", "You really need to do better"];
-	var score;
+// Q3
+	if (question3 == "brunch") {
+					a++;
+}
+	if (question3 == "comedy") {
+					b++;
+}
+	if (question3 == "trendy") {
+					c++;
+}
+	if (question3 == "wine") {
+					d++;
+}
+	if (question3 == "dinner") {
+					e++;
+}
 
-	if (correct == 0) {
-		score = 2;
-	}
+// Q4
+	if (question4 == "entrepreneur") {
+					a++;
+}
+	if (question4 == "musician") {
+					b++;
+}
+	if (question4 == "athlete") {
+					c++;
+}
+	if (question4 == "lawyer") {
+					d++;
+}
+	if (question4 == "writer") {
+					e++;
+}
 
-	if (correct > 0 && correct < 3) {
-		score = 1;
-	}
+// Q5
+	if (question5 == "monkey") {
+					a++;
+}
+	if (question5 == "dog") {
+					b++;
+}
+	if (question5 == "dolphin") {
+					c++;
+}
+	if (question5 == "fox") {
+					d++;
+}
+	if (question5 == "cat") {
+					e++;
+}
 
-	if (correct == 3) {
-		score = 0;
-	}
+//image
+var pictures = ["../img/toby1.png", "../img/toby2.png", "../img/toby3.png"];
+var messages = ["strawberry", "banana", "watermelon", "lemon", "pear"];
+var score;
 
-	document.getElementById("after_submit").style.visibility = "visible";
+// guidelines
+let max = a;
+if (b>max) {
+	max = b;
+}
+if (c>max) {
+	max = c;
+}
+if (d>max) {
+	max = d;
+}
+if (e>max) {
+	max = e;
+}
 
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
+// score
+if (max == a) {
+	score = 0;
+}
+
+if (max == b) {
+	score = 1;
+}
+
+if (max == c) {
+	score = 2;
+}
+
+if (max == d) {
+	score = 3;
+}
+
+if (max == e) {
+	score = 4;
+}
+
+//results
+document.getElementById("after_submit").style.visibility = "visible";
+document.getElementById("number_correct").innerHTML = "you are a " + messages[score] + "!";
+document.getElementById("picture").src = pictures[score];
+
 	}
